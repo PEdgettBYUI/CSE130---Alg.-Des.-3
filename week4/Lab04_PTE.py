8fac8b5611e1f75be21f77e567874ed49d8d02d6
ContraZombie4 <102491665+ContraZombie4@users.noreply.github.com>
2026-05-16 15:40:34 -0600

    Monopoly lab working, completed, with breaks (Will revise from here);

--- week4/Lab04_PTE.py
+++ week4/Lab04_PTE.py
@@ -12,10 +12,25 @@
 # 5. How long did it take for you to complete the assignment?
 #      -total time in hours including reading the assignment and submitting the program-
 
-
+# Function to display the 4 possible outputs for Purchasing a hotel.
+def purchase_results(price, houses_on_NC, houses_on_PC, total_houses):
+    # Find out how many houses are needed for North Carolina
+    houses_for_NC = 4 - houses_on_NC
+    # Find out how many houses are needed for Pacific Avenue
+    houses_for_PC = 4 - houses_on_PC
+    print(f"\nThis will cost ${price}.")
+    print(f"    Purchase 1 hotel and {total_houses} house(s).")
+    print("    Put 1 hotel on Pennsylvania and return any houses to the bank.")
+    if(houses_for_NC > 0):
+        print(f"Put {houses_for_NC} house(s) on North Carolina.")
+    if(houses_for_PC > 0):
+        print(f"Put {houses_for_NC} house(s) on Pacific.")
 
 # Initialize variables.
 total_houses_on_green = 0
+houses_on_PA = 0
+houses_on_NC = 0
+houses_on_PC = 0
 hotels_in_bank = 0
 houses_in_bank = 0
 cash_available = 0
@@ -31,11 +46,12 @@ while start == 'y':
 
     # Prompt the user to confirm whether they own all green properties.
     # If false, output: No Properties.
-    user_input = str(input("Do you own all the green properties? (y/n) "))
+    user_input = str(input("Do you own all the green properties? (y/n) ")).lower()
     if (user_input == "n"):
-        print("You cannot purchase a hotel until you own" \
+        print("\nYou cannot purchase a hotel until you own" \
         "all the properties of a given color group.")
         break
+        
 
 
     # Prompt the user to enter what is currently on Pennsylvania Avenue.
@@ -45,11 +61,12 @@ while start == 'y':
     # If the user_input variable is between 0 and 5, 
     # add the value to the total_houses_on_green counter
     if(user_input > 0 or user_input < 5):
-        total_houses_on_green += user_input
+        houses_on_PA += user_input
+
 
     # If 5, output: Already has Hotel.
     if (user_input == 5):
-        print("You cannot purchase a hotel if the property already has one.")
+        print("\nYou cannot purchase a hotel if the property already has one.")
         break
 
 
@@ -60,11 +77,11 @@ while start == 'y':
     # If the user_input variable is between 0 and 5, 
     # add the value to the total_houses_on_green counter
     if(user_input > 0 or user_input < 5):
-        total_houses_on_green += user_input
+        houses_on_PC += user_input
 
     # If 5, output: Swap PC Hotel.
     if (user_input == 5):
-        print("Swap Pacific's hotel with Pennsylvania's 4 houses.")
+        print("\nSwap Pacific's hotel with Pennsylvania's 4 houses.")
         break
 
     # Prompt the user to enter what is currently on North Carolina Avenue.
@@ -74,34 +91,42 @@ while start == 'y':
     # If the user_input variable is between 0 and 5, 
     # add the value to the total_houses_on_green counter
     if(user_input > 0 or user_input < 5):
-        total_houses_on_green += user_input
+        houses_on_NC += user_input
 
     # If 5, output: Swap: NC Hotel.
     if (user_input == 5):
-        print("Swap North Carolina's hotel with Pennsylvania's 4 houses.")
+        print("\nSwap North Carolina's hotel with Pennsylvania's 4 houses.")
         break
 
-
-    # Calculate needed_houses by finding the difference.
-    needed_houses = 12 - total_houses_on_green
-    cash_needed = 200 * needed_houses
     # Prompt the user to enter how many hotels are available for purchase from the bank.
     hotels_in_bank = int(input("How many hotels are there to purchase? "))
     # If hotels_in_bank < 1, output: No Hotels.
     if (hotels_in_bank < 1):
-        print("There are not enough hotels available for purchase at this time.")
+        print("\nThere are not enough hotels available for purchase at this time.")
         break
 
+
+    # Calculate the total houses on green.
+    total_houses_on_green = houses_on_PA + houses_on_NC + houses_on_PC
+    # Calculate the number of houses needed to buy 1 hotel for PA Avenue.
+    needed_houses = 12 - total_houses_on_green
+    #  Calculate the amount of cash needed to buy 1 hotel on PA Avenue.
+    cash_needed = (200 * needed_houses) + 200
+
+    
     # Prompt the user to confirm whether the bank has enough houses for sale.
     houses_in_bank = int(input("How many houses are there to purchase? "))
     # If houses_in_bank < needed_houses, output: Not Enough Houses.
     if (houses_in_bank < needed_houses):
-        print("There are not enough houses available for purchase at this time.")
+        print("\nThere are not enough houses available for purchase at this time.")
         break
 
     # Prompt the user to enter how much cash they have available.
     cash_available = int(input("How much cash do you have to spend? "))
     # If cash < (200 * needed_houses), the user cannot afford the purchase.
     if(cash_available < cash_needed):
-        print("You do not have sufficient funds to purchase a hotel at this time.")
+        print("\nYou do not have sufficient funds to purchase a hotel at this time.")
         break
+
+    # Display the final purchase results
+    purchase_results(cash_needed, houses_on_NC, houses_on_PA, total_houses_on_green)
