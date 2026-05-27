dda4bd69141b9115b469ee2862bb3bf668aa9b84
Patrick Edgett <edg24004@byui.edu>
2026-05-27 16:42:48 -0600

    Added the required and testing .json files; Added the header with some completion to decompress_image.py;

---
+++ week6/decompress_image.py
@@ -0,0 +1,17 @@
+# 1. Name:
+#      Patrick T. Edgett
+# 2. Assignment Name:
+#      Lab 06: Image (de)Compression
+# 3. Assignment Description:
+#      This program decompresses data from a .JSON 
+#       file and outputs an image using ASCII characters.
+# 4. Algorithmic Efficiency
+#      The Algorithm is a O(n^2) efficiency. 
+#       This is because the decoding process requires us to go through every
+#       value in a 2-Dimensional array, and we need to repeat this process to
+#       display the image; both of which must use a nested for-loop.
+# 5. What was the hardest part? Be as specific as possible.
+#      -a paragraph or two about how the assignment went for you-
+# 6. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and submitting the program-
+
